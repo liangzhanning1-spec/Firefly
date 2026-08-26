42ab819f65552d03380d19542b6c199ce4c89abf
宁 <ningnb1102@gmail.com>
2026-08-26 14:37:59 +0800

    Update sponsorConfig.ts

--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -23,12 +23,12 @@ export const sponsorConfig: SponsorConfig = {
 	// 打赏方式列表
 	methods: [
 		{
-			name: "支付宝",
+			name: "微信",
 			icon: "fa7-brands:alipay",
 			// 收款码图片路径（需要放在 public 目录下）
-			qrCode: "/assets/images/sponsor/alipay.png",
+			qrCode: "/assets/images/sponsor/weixin.png",
 			link: "",
-			description: "使用 支付宝 扫码打赏",
+			description: "使用 微信 扫码打赏",
 			enabled: true,
 		},
 		{
@@ -39,38 +39,22 @@ export const sponsorConfig: SponsorConfig = {
 			description: "使用 微信 扫码打赏",
 			enabled: true,
 		},
-		{
-			name: "ko-fi",
-			icon: "simple-icons:kofi",
-			qrCode: "",
-			link: "https://ko-fi.com/cuteleaf",
-			description: "Buy a Coffee for Firefly",
-			enabled: true,
-		},
-		{
-			name: "爱发电",
-			icon: "simple-icons:afdian",
-			qrCode: "",
-			link: "https://ifdian.net/a/cuteleaf",
-			description: "通过 爱发电 进行打赏",
-			enabled: true,
-		},
 	],
 
 	// 打赏者列表（可选）
 	sponsors: [
 		// 示例：已实名打赏者
 		{
-			name: "夏叶",
+			name: "测试1",
 			avatar:
-				"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
+				"",
 			amount: "¥50",
 			date: "2025-10-01",
 		},
 
 		// 示例：匿名打赏者
 		{
-			name: "匿名用户",
+			name: "测试2",
 			// avatar: "",
 			amount: "¥20",
 			date: "2025-10-01",
