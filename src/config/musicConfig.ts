85177c25a7ebdba7912d33f118e6a85641358ce8
宁 <ningnb1102@gmail.com>
2026-08-26 14:01:47 +0800

    Update musicConfig.ts

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -48,9 +48,9 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	local: {
 		playlist: [
 			{
-				name: "使一颗心免于哀伤",
-				artist: "知更鸟 / HOYO-MiX / Chevy",
-				url: "/assets/music/使一颗心免于哀伤-哼唱.mp3",
+				name: "失眠",
+				artist: "宁",
+				url: "/assets/music/失眠.mp3",
 				cover: "/assets/music/cover/109951169585655912.webp",
 				lrc: "",
 			},
